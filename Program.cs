0e6593e4337b34624ae63cdd317c5189e8095c93
Utso Sarkar <87900549+Utso-Sarkar@users.noreply.github.com>
2022-10-23 23:38:20 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "My name";
+            Console.WriteLine(name);
+            int first = 10;
+            int secound = 20;
+            int result = first + secound;
+            Console.WriteLine(result);
+        }
+    }
+}
